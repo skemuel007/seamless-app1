6b07f4b3a56c60d3f3baaabbd4110f201a15f24a
skemuel007 <skemuel007@gmail.com>
2020-02-22 21:44:57 +0100

    Updated auth controller to return user details

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -103,4 +103,15 @@ class AuthController extends Controller
             'data' => null,
         ], 200);
     }
+
+    /**
+     * Get the authenticated User.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function me()
+    {
+        return response()->json(auth()->user());
+    }
+
 }
