c0eb450985ee58ad107734250de629f37114f7d8
skemuel007 <skemuel007@gmail.com>
2020-02-22 21:46:20 +0100

    Change method name from me to profile

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -109,7 +109,7 @@ class AuthController extends Controller
      *
      * @return \Illuminate\Http\JsonResponse
      */
-    public function me()
+    public function profile()
     {
         return response()->json(auth()->user());
     }
