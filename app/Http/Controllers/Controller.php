dcd097d6fbd26dbe3a9dac7a9d9f7b57e13d911a
skemuel007 <skemuel007@gmail.com>
2020-02-23 00:19:12 +0100

    Swagger documentation comment added to root controller

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -7,6 +7,24 @@ use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
+/**
+ * Class Controller
+ * @package App\Http\Controllers
+ *
+ * @SWG\Swagger(
+ *     basePath="/api",
+ *     host="localhost:8000",
+ *     schemes={"https", "http"},
+ *     @SWG\Info(
+ *          version="1.0",
+ *          title="Seamless API test",
+ *          @SWG\Contact(
+ *              name="Stanley-Kemuel Lloyd Salvation"
+ *          ),
+ *     )
+ * )
+ *
+ */
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
