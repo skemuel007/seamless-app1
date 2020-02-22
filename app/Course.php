cb56d8768754b22536fbdff9660b334522359da1
skemuel007 <skemuel007@gmail.com>
2020-02-22 21:40:11 +0100

    Course model updated with properties and create course method created in CourseController

--- app/Course.php
+++ app/Course.php
@@ -7,4 +7,17 @@ use Illuminate\Database\Eloquent\Model;
 class Course extends Model
 {
     //
+    protected $fillable = [
+        'course_name',
+        'course_code',
+        'units'
+    ];
+
+    protected $guarded = [
+        'id', 'created_at', 'updated_at'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
 }
--- app/Http/Controllers/CourseController.php
+++ app/Http/Controllers/CourseController.php
@@ -2,9 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProcessCourse;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class CourseController extends Controller
 {
     //
+    public function createCourse() {
+       // create job queue with a 5 seconds delay
+        $createCourseJob = (new ProcessCourse())
+            ->delay(Carbon::now()->addSeconds(5))->onQueue('processing');
+        $this->dispatch($createCourseJob); // dispatch the job
+
+        // return response
+        return response()->json([
+            'message' => 'Courses created',
+            'data' => []
+        ]);
+    }
 }
