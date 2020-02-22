9ac2510651c86a054ae1c81824bd2a671a09e4b4
skemuel007 <skemuel007@gmail.com>
2020-02-22 22:33:50 +0100

    Model updated with fillable, guarded and hidden properties

--- app/CourseRegistration.php
+++ app/CourseRegistration.php
@@ -6,5 +6,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class CourseRegistration extends Model
 {
-    //
+    // fillable properties
+    protected $fillable = [
+        'user_id',
+        'course_id',
+    ];
+
+    // guarded properties from mass fillable
+    protected $guarded = [
+        'id', 'created_at', 'updated_at'
+    ];
+
+    protected $hidden = [
+        'updated_at'
+    ];
 }
