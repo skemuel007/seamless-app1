3389c4f936bad546b8abefe7d2b963e85184efe8
skemuel007 <skemuel007@gmail.com>
2020-02-22 18:51:48 +0100

    Migration updated

--- database/migrations/2020_02_22_174524_add_columns_to_course_table.php
+++ database/migrations/2020_02_22_174524_add_columns_to_course_table.php
@@ -14,7 +14,10 @@ class AddColumnsToCourseTable extends Migration
     public function up()
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            // three (3) new columns defined for courses table
+            $table->string('course_name')->unique();
+            $table->string('course_code')->unique();
+            $table->bigInteger('unit')->default(3);
         });
     }
 
@@ -26,7 +29,14 @@ class AddColumnsToCourseTable extends Migration
     public function down()
     {
         Schema::table('courses', function (Blueprint $table) {
-            //
+            // drop tables when migration is reversed
+            $table->dropUnique('course_name');
+            $table->dropUnique('course_code');
+        });
+
+        Schema::table('courses', function (Blueprint $table) {
+            // drop tables when migration is reversed
+            $table->dropColumn('unit');
         });
     }
 }
