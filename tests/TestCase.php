3d30d16c5dc1fa88bcdcc1bfbd7d38308e9bc52f
skemuel007 <skemuel007@gmail.com>
2020-02-23 11:40:14 +0100

    LoginTest updated, create Register Test

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,25 @@
 
 namespace Tests;
 
+use Faker\Factory;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    protected $faker;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        // Artisan::call('db:seed');
+        // Artisan::call('migrate:fresh --env=testing');
+        $this->faker = Factory::create();
+    }
+
+    public function tearDown(): void
+    {
+        parent::tearDown();
+    }
 }
--- tests/Unit/LoginTest.php
+++ tests/Unit/LoginTest.php
@@ -2,19 +2,61 @@
 
 namespace Tests\Unit;
 
+use App\User;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+class MyClass {
+
+}
 class LoginTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
-    {
-        $this->assertTrue(true);
+    public function test_requires_email_password() {
+        $response = new \stdClass();
+        $response->message = 'Parameter validation failure';
+
+        $errors = new MyClass();
+        $errors->email = ['The email field is required.'];
+        $errors->password = ['The password field is required.'];
+
+        $response->error = $errors;
+
+        $this->json('POST', '/api/login')
+            ->assertStatus(422);
+    }
+
+    public function test_user_logs_in_successfully() {
+        $email = $this->faker->email;
+        $applicant = factory(User::class)->create([
+            'email' => $email,
+            'name' => $this->faker->name,
+            'password' => bcrypt('12345'),
+        ]);
+        $payload = ['email' => $email, 'password' => '12345'];
+
+        $this->json('POST', '/api/login', $payload)
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'message',
+                'token'
+            ]);
+    }
+
+    public function test_logs_in_invalid_credentials() {
+
+        $payload = [
+            'email' => $this->faker->unique()->safeEmail,
+            'password' => '12345'
+        ];
+
+        $this->json('POST', '/api/login', $payload)
+            ->assertStatus(401)
+            ->assertJsonStructure(
+                [
+                    'message',
+                    'error'
+                ]
+            );
     }
 }
