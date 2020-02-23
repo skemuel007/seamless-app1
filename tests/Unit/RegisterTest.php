5a77ae0284f7e25e6cf34d998f1d6b911f77a5df
skemuel007 <skemuel007@gmail.com>
2020-02-23 12:49:37 +0100

    Registration Test updated

--- tests/Unit/RegisterTest.php
+++ tests/Unit/RegisterTest.php
@@ -2,6 +2,8 @@
 
 namespace Tests\Unit;
 
+use App\User;
+use Illuminate\Support\Facades\DB;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -25,5 +27,39 @@ class RegisterTest extends TestCase
         $this->post('/api/register', $data)
             ->assertStatus(201)
             ->assertJson($response);
+    }
+
+    public function test_invalid_credentials_registration() {
+
+        $this->json('POST', '/api/register')
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'message',
+                'error'
+            ]);
+    }
+
+    public function test_post_already_registered_user() {
+
+        $applicant = factory(User::class)->create([
+            'email' => 'skemuel@gmail.com',
+            'name' => 'Salvation Lloyd Stanley-Kemuel',
+            'password' => bcrypt('12345'),
+        ]);
+
+        $data = [
+            'email' => 'skemuel@gmail.com',
+            'name' => 'Salvation Stanley-Kemuel',
+            'password' => bcrypt('12345')
+        ];
+
+        $response = [
+            "message",
+            "error"
+        ];
+
+        $this->json('POST','/api/register', $data)
+            ->assertStatus(422)
+            ->assertJsonStructure($response);
     }
 }
